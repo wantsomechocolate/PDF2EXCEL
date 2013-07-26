2de0533b7fb77141355a549858d3ee0e57cc3d08
James McGlynn <wantsomechocolate@gmail.com>
2013-07-26 16:33:31 -0400

    Comparing Test Based Images

--- compare_test.py
+++ compare_test.py
@@ -15,9 +15,12 @@ from marbles import glass as useful
 ## IMPORT TIME SO KNOW HOW LONG STUFF TAKES
 from time import time
 
+from openpyxl import Workbook
+from openpyxl import load_workbook
+
 
 ## TESTING DIRECTORY
-wdir="C:/Users/James McGlynn/My Programs/Python Programs/pdf2txt/CompareExperiment/compare/images"
+wdir="C:/Users/James McGlynn/My Programs/Python Programs/pdf2txt/CompareExperiment/compare/images/"
 
 ## GET A LIST OF PNG IMAGES FROM A GIVEN DIRECTORY
 image_handle_list=useful.image_handles_from_dir_png(wdir)
@@ -72,14 +75,36 @@ for i in range(len(image_handle_list)):
 
 time3=time()
 print "Comparing all images took ",
-print (time3-time2)/float(60),
-print " minutes"
+print (time3-time2),
+print " seconds"
 
 ## KEEP THIS AROUND FOR EASY PRINTING OF COMPARISON RESULTS
 ##for item in image_comparison_list:
 ##	print os.path.basename(item[0].filename),
 ##	print os.path.basename(item[1].filename),
 ##	print item[2]
+
+book_name=wdir+'compare.xlsx'
+wb = Workbook()
+ws=wb.get_active_sheet()
+
+c=ws.cell(row=0, column=0)
+c.value="Image One"
+c=ws.cell(row=0, column=1)
+c.value="Image Two"
+c=ws.cell(row=0, column=2)
+c.value="Similarity"
+
+for i in range(len(image_comparison_list)):
+    #for j in range(len(image_comparison_list[i])):
+    c=ws.cell(row=i+1, column=0)
+    c.value=os.path.basename(image_comparison_list[i][0].filename)
+    c=ws.cell(row=i+1, column=1)
+    c.value=os.path.basename(image_comparison_list[i][1].filename)
+    c=ws.cell(row=i+1, column=2)
+    c.value=image_comparison_list[i][2]
+
+wb.save(book_name)
 
 
 
@@ -89,8 +114,69 @@ compare.compare_results_one_image(image_comparison_list,im2compare,thresh)
 			
 
 
-
-
+##def print_to_workbook(data_order_list,dict_to_print,source_path,tab_name):
+##    
+##    ## Openpyxl library imports
+##    from openpyxl import Workbook
+##    from openpyxl import load_workbook
+##
+##    ## Get the excel filename from the name of the text file that the user navigated to
+##    book_name=source_path+'compare.xlsx'#jc.getFilenameFromPath(source_path)+'.xlsx'
+##
+##    try: ## Try opening the workbook with the same name"
+##        #Found Workbook
+##        wb=load_workbook(book_name)
+##        new_wb=-1
+##        try:
+##            # Found tab
+##            ws = wb.get_sheet_by_name(tab_name) #this does not error if it doesn't find what it wants
+##            if ws==None:
+##                1+"one"#throw error"
+##            else:
+##                pass
+##        except:
+##            #Found workbook, but didn't find tab"
+##            ws=wb.create_sheet(-1,tab_name)
+##            ws=wb.get_sheet_by_name(tab_name)
+##            new_wb=-1
+##            i=0
+##            for key in data_order_list['library_info']['collection_order']:
+##                c=ws.cell(row=0, column=i)
+##                c.value=key
+##                i=i+1
+##        
+##    except: ## If it doesn't exist then start a new workbook in memory
+##        #Didn't find workbook
+##        wb = Workbook()
+##        ws = wb.create_sheet(-1,tab_name)
+##        ws = wb.get_sheet_by_name(tab_name)
+##        new_wb=1
+##        i=0
+##        for key in data_order_list['library_info']['collection_order']:
+##            c=ws.cell(row=0, column=i)
+##            c.value=key
+##            i=i+1
+##        
+##    last_occ_row=ws.rows[-1][0].row
+##    
+##
+##    i=0
+##    for key in data_order_list['library_info']['collection_order']:
+##
+##        c=ws.cell(row=last_occ_row, column=i)
+##        try:
+##            c.value=dict_to_print[key].strip()
+##        except:
+##            #Using this area to keep track of encoding errors
+##            print "This text caused an error"
+##            print dict_to_print[key]
+##            c.value="Raw text contained bad chars, see intepreter."
+##            #c.value=dict_to_print[key]
+##        i=i+1
+##            
+##    wb.save(book_name)
+##    return book_name
+##
 
 
 
